00c06b87ceebcf92aada56ddaacf3c57a1bda3f4
kulas115 <kulas115@gmail.com>
2017-11-01 22:14:04 +0100

    Finish user edit, update, index and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:kamil)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -9,6 +14,17 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", login_path
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", login_path, count: 0
     get contact_path
     assert_select "title", full_title("Contact")
     get signup_path
---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,53 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:kamil)
+  end
+
+  test "unsuccessful edit" do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: { user: { name: "",
+                                              email: "foo@invalid",
+                                              password: "foo",
+                                              password_confirmation: "bar" } }
+    assert_select "div.alert"
+    assert_template 'users/edit', "the form contains 4 errors"
+  end
+
+  test "successful edit" do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: { user: { name: name,
+                                              email: email,
+                                              password: "", # password is blank as in this case user do not wanted to update it
+                                              password_confirmation: "" } }
+    assert_not flash.empty? # Checking for non empty flash message.
+    assert_redirected_to @user # Redirecting to user page
+    @user.reload # reload user's values from the db and confirm that they were successfully updated.
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
+
+  test "successful edit with friendly forwarding" do
+    get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
+    name = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: { user: { name: name,
+                                              email: email,
+                                              password: "",
+                                              password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name,  @user.name
+    assert_equal email, @user.email
+  end
+end
