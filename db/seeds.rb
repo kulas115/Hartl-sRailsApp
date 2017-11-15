5b747a1fa377f51da920b2736dab1444c5400702
kulas115 <kulas115@gmail.com>
2017-11-15 21:54:33 +0100

    Work on micropost image uploader

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,6 +21,11 @@ User.create!(name:  "Example User",
                email: email,
                password:              password,
                password_confirmation: password,
-               activated: true,
-               activated_at: Time.zone.now)
+               activated: true)
+end
+
+users = User.order(:created_at).take(6)
+50.times do
+ content = Faker::Lorem.sentence(5)
+ users.each { |user| user.microposts.create!(content: content) }
 end
