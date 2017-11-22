6a8448dabefa006b1303289826aa2dbcf7610207
kulas115 <kulas115@gmail.com>
2017-11-22 21:23:37 +0100

    Add user following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -92,4 +92,22 @@ class UserTest < ActiveSupport::TestCase
     kamil.unfollow(archer)
     assert_not kamil.following?(archer)
   end
+
+  test "feed should have the right posts" do
+    kamil  = users(:kamil)
+    archer = users(:archer)
+    lana   = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert kamil.feed.include?(post_following)
+    end
+    # Post from self
+    kamil.microposts.each do |post_self|
+      assert kamil.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not kamil.feed.include?(post_unfollowed)
+    end
+  end
 end
